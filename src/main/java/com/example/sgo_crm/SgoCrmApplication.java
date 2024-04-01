d59d39b46c5e5b2d347746da8b4d0026522fe267
NHAT <thanhdeapool@gmail.com>
2024-04-01 14:57:26 +0700

    first commit

---
+++ src/main/java/com/example/sgo_crm/SgoCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.sgo_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgoCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgoCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sgo_crm/SgoCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sgo_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgoCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
