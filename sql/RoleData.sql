7d20a290ae92f1152fe140136d72720c921fa29f
l3bao84 <l3ducb4008@gmail.com>
2024-04-03 14:01:32 +0700

    add file roles data

---
+++ sql/RoleData.sql
@@ -0,0 +1,4 @@
+insert into roles values
+                      (1, "Quản Trị Viên (Admin) quản lý và bảo trì hệ thống, đồng thời đảm bảo an ninh. Họ có quyền toàn diện để cập nhật, giải quyết sự cố và quản lý dữ liệu, đảm bảo hệ thống hoạt động ổn định và an toàn.", "Admin"),
+                      (2, "Tập trung vào việc tạo chiến dịch quảng cáo và tăng nhận thức thương hiệu để thu hút khách hàng.", "Sale Employee"),
+                      (3, "Chịu trách nhiệm bán sản phẩm/dịch vụ, tư vấn và duy trì mối quan hệ với khách hàng để đạt mục tiêu doanh số.", "Marketing Employee")
