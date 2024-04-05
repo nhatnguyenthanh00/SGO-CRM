da67a685b448a5b80cf37b8f424d4aec7fc0304a
l3bao84 <l3ducb4008@gmail.com>
2024-04-05 16:41:53 +0700

    dev xem danh sách fb ad  page

--- src/main/java/com/example/sgo_crm/controller/PageController.java
+++ src/main/java/com/example/sgo_crm/controller/PageController.java
@@ -1,9 +1,26 @@
 package com.example.sgo_crm.controller;
 
+import com.example.sgo_crm.model.APIResponse;
+import com.example.sgo_crm.service.impl.FbAdPageServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/pages")
 public class PageController {
+
+    private final FbAdPageServiceImpl fbAdPageService;
+
+    public PageController(FbAdPageServiceImpl fbAdPageService) {
+        this.fbAdPageService = fbAdPageService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> getfbAdPages(@RequestParam(defaultValue = "0", required = false) int page) {
+        APIResponse apiResponse = fbAdPageService.getFbAdPages(page);
+        return ResponseEntity.ok().body(apiResponse);
+    }
 }
